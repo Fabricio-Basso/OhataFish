d3970d0d5dac7a6cc3a9da8a35b9e5d86e746ccb
fabricio-basso <fabricio.basso12@gmail.com>
2021-05-03 20:51:15 -0300

    first commit

---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{    
+    //Velocidade do peixe
+    float speed;
+
+    private void Start()
+    {
+        //Puxando speed diretamente do FlockManager através de um SINGLETON!! =D
+        speed = Random.Range(FlockManager.instance.minSpeed, FlockManager.instance.maxSpeed);
+    }
+
+    private void Update()
+    {
+        //Movimentação do peixe
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/FlockManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    //Criando um Singleton
+    #region SINGLETON
+    public static FlockManager instance;
+    private void Awake()
+    {
+        if(!instance)
+        {
+            instance = this;
+        }
+    }
+    #endregion
+
+    //Prefab do peixe, Array com todos os peixes da Scene.
+    public GameObject fishPrefab;
+    public GameObject[] allFish;
+
+    //Num total de peixes a serem instanciados.
+    public int numFish = 20;
+
+    //Vector3 que será utilizado para calcular o futuro Spawn dos peixes.
+    public Vector3 swinLimits = new Vector3(5, 5, 5);
+
+    //Velocidade
+    public float minSpeed, maxSpeed;
+
+    private void Start()
+    {
+        //Set qnt de peixes na Scene
+        allFish = new GameObject[numFish];
+
+        //Instanciando os peixes
+        for (int i = 0; i < numFish; i++)
+        {
+            //Setando Posicoes RANDOM
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x),
+                                                                Random.Range(-swinLimits.y, swinLimits.y),
+                                                                Random.Range(-swinLimits.z, swinLimits.z));
+            //Instanciando os peixes nas posições RANDOM de cima (:
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+        }
+    }
+}
